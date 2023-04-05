9780c7a38d3ec7e5a1bd144432a4b35ef1e9bae4
Traniece Johnson <traniecejohnson@Tranieces-MacBook-Pro-2.local>
2023-04-05 12:02:58 -0400

    made some changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   useEffect(() => {
     fetch('https://backend-deploy-xvdb.onrender.com')
       .then((res) => res.json())
-      .then((name) => setName([name]))
+      .then((nme) => setName([nme]))
   }, [])
 
   return (
